462a4b4db9d2558aa7f0a472ec73bf662730864b
Abdulqadir Ahmad <arfs6.mail@gmail.com>
2023-10-01 15:09:59 +0100

    changed working dir of build script

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,3 +1,22 @@
 # Build a TW Blue installer.
-cd .. # Go to root of repo
-pip install -r requirements.txt
+# Most be called from root of repo
+echo "Installing python packages..."
+cd ..
+python -m pip install -r requirements.txt
+echo "done."
+
+echo "Generating documentation..."
+cd doc
+python document_importer.py
+python generator.py
+mv license.txt documentation
+mv documentation ..\src
+cd ..
+echo "done."
+
+echo "Building binary..."
+cd src
+python setup.py build
+}
+cd ..
+echo "done."
