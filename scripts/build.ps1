baa4889fc762c3a3979e7b96323ceee1dbca45b8
Abdulqadir Ahmad <arfs6.mail@gmail.com>
2023-12-28 09:55:45 +0100

    fixed error in build script

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,15 +1,10 @@
 # Build a TW Blue installer.
 # Must be called from root of repo
-echo "Installing python packages..."
-pwd
-pip install -r requirements.txt
-echo "done."
-
 echo "Generating documentation..."
 cd doc
 python document_importer.py
 python generator.py
-mv license.txt documentation
+mv ..\license.txt documentation
 mv documentation ..\src
 cd ..
 echo "done."
