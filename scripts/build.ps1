a3d3a6f9e03a4191781669ef9632104509cedd83
Abdulqadir Ahmad <arfs6.mail@gmail.com>
2023-12-28 13:54:40 +0100

    fixed a problem in build script

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -2,7 +2,7 @@
 # Must be called from root of repo
 echo "Generating documentation..."
 cd doc
-python document_importer.py
+python documentation_importer.py
 python generator.py
 mv ..\license.txt documentation
 mv documentation ..\src
